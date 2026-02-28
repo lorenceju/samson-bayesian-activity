6069a41a1999e1ed1f1e5a05a0a6816cc70e2c35
Julia Lorence Samson <77075183+lorenceju@users.noreply.github.com>
2026-03-01 00:07:23 +0800

    Update bayesians-samson.py
    Added Bayesian analysis with uniform and beta distributions, likelihood function, and posterior calculations.

--- bayesians-samson.py
+++ bayesians-samson.py
@@ -1,1 +1,45 @@
+import scipy.stats as sts
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Prior
+
+mu = np.linspace(1.65, 1.8, num = 50)
+test = np.linspace(0, 2)
+uniform_dist = sts.uniform.pdf(mu) + 1
+
+uniform_dist = uniform_dist/uniform_dist.sum()
+beta_dist = sts.beta.pdf(mu, 2, 5, loc = 1.65, scale = 0.2)
+beta_dist = beta_dist/beta_dist.sum()
+
+plt.plot(mu, beta_dist, label = 'Beta Dist')
+plt.plot(mu, uniform_dist, label = 'Uniform Dist')
+plt.xlabel("Value of $\\mu$ in meters")
+plt.ylabel("Probability Density")
+plt.legend()
+plt.show()
+
+# Likelihood
+
+def likelihood_func(datum, mu):
+    likelihood_out = sts.norm.pdf(datum, mu, scale = 0.1)
+    return likelihood_out/likelihood_out.sum()
+
+likelihood_out = likelihood_func(1.7, mu)
+
+plt.plot(mu, likelihood_out)
+plt.title("Likelihood of $\\mu$ give observation 1.7m")
+plt.ylabel("Probability Density/Likelihood")
+plt.xlabel("Value of $\\mu$")
+plt.show()
+
+# Evidence
+
+unnormalized_posterior = likelihood_out * uniform_dist
+plt.plot(mu, unnormalized_posterior)
+plt.xlabel("$\\mu$ in meters")
+plt.ylabel("Unnormalized Posterior")
+plt.show()
+
+unnormalized_posterior.sum()
 
